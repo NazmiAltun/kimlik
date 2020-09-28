fbb392c726b01362f57c3ba7a118bd233278af68
Nazmi Altun <nazmialtun@windowslive.com>
2020-09-29 02:12:52 +0800

    Code Smells

--- src/Kimlik/Program.cs
+++ src/Kimlik/Program.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Kimlik
 {
     public class Program
     {
+        protected Program()
+        {
+        }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
